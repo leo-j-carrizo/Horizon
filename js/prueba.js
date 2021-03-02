6baedd8ca1a511047cc19b38ea3855acf4a40d17
Joako20 <leo_j_carrizo@hotmail.com>
2021-03-02 05:20:00 -0300

    prueba conexion con json

--- js/prueba.js
+++ js/prueba.js
@@ -1,7 +1,29 @@
-let botonCompra = $(".price").next()
+let botonCompra = $(".price").next();
 
-let baseProductos = $.get()
+let baseProductos = $.get("https://joako20.github.io/Horizon/json/productos.json");
+
+let tarjeta = $(".card");
 
 botonCompra.click((e)=>{
 e.preventDefault()
+
 })
+
+
+tarjeta.click(
+    function(){
+        
+        let nose = baseProductos.responseJSON[0].id
+        for(var i = $(this).data("id"); i >= nose;nose++){
+            if(nose = i){
+
+                alert(baseProductos.responseJSON[i].nombre);
+                break;
+            }
+            else{
+                alert("no")
+            }
+        }
+    }
+)
+
