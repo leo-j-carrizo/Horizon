d3156e49dc5224e6227278b148b1408b81d3f35e
Joako20 <leo_j_carrizo@hotmail.com>
2021-03-02 22:11:52 -0300

    descripcion de productos y html dinamico maquetado

--- js/prueba.js
+++ js/prueba.js
@@ -16,7 +16,7 @@ tarjeta.click(
         let dataId = $(this).data("id")
         
         
-        for(var i = 0; dataId >= baseProductos.responseJSON[i].id;i++){
+        for(var i = 0; dataId < baseProductos.responseJSON.length;i++){
             if(baseProductos.responseJSON[i].id == dataId){
                 alert(baseProductos.responseJSON[i].nombre,)
                 break
